ff730e94bb7f226c3abe6e1464b44fff4a8eab38
Tanguri <jungwontwo2@naver.com>
2024-02-07 09:53:53 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("fixDiscountPolicy")   Qualifier랑 Primary랑 같이 나오면 더 자세하고 수동인 Qualifier가 우선권 가짐
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount=1000;
     @Override
